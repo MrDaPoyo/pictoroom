8659db38bc4333afec176fb840ad78762f0196dc
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-11-24 09:31:04 +0000

    users

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Store the feed of drawings
 const drawings = [];
+
+app.get('/chat/:room', (req, res) => {
+    res.render('chatroom', {room: req.params.room, username: req.query.username});
+});
+
+app.post('/joinRoom', async (req, res) => {
+    res.redirect(`/chat/${await req.body.room}?username=${req.body.username}`);
+});
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -22,15 +35,21 @@ io.on('connection', (socket) => {
     // Send existing drawings to the newly connected user
     socket.emit('initializeFeed', drawings);
 
-    // Handle new drawing submissions
-    socket.on('sendDrawing', (drawingData) => {
+    // Join a room
+    socket.on('joinRoom', (room) => {
+        socket.join(room);
+        console.log(`User ${socket.id} joined room ${room}`);
+    });
+
+    // Handle new drawing submissions for a specific room
+    socket.on('sendDrawing', (room, drawingData) => {
         // Add drawing to the feed
         if (drawingData && Object.keys(drawingData).length > 0) {
             drawings.push(drawingData);
         }
 
-        // Broadcast the new drawing to all users
-        io.emit('newDrawing', drawingData);
+        // Broadcast the new drawing to all users in the room
+        io.to(room).emit('newDrawing', drawingData);
     });
 
     socket.on('disconnect', () => {
