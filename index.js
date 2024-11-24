507d6a0bbc498122f34fb7e977e9233d0878ef90
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-11-24 07:51:23 +0000

    routes

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    
+    socket.on('canvas data', (data) => {
+        io.emit('canvas data', data);
+    });
+});
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/chat', (req, res) => {
+    res.redirect('/');
+});
+
+app.get('/chat/:room', (req, res) => {
+    const room = req.params.room;
+    const name = req.query.name;
+    if (!name) {
+        return res.redirect('/');
+    }
+    res.render('chat', { room, name });
+});
