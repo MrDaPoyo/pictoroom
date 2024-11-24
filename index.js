ecf73d3be54b82ee4c60302eef8fac2e3ebfb768
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-11-24 07:53:06 +0000

    views

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 io.on('connection', (socket) => {
     console.log('a user connected');
